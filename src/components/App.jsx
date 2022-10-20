025158c92b4d827a60ca09e1f9168f7c519744d8
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-10-20 13:53:55 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,19 @@
-export const App = () => {
-  return (
-    <div
+import React from "react";
+import Input from "../components/Input.jsx";
+
+export class App extends React.Component  {
+  state = {
+  contacts: [],
+  name: ''
+}
+  
+  render() {
+    
+    const name = this.state;
+    const contacts = this.state;
+
+    return (
+      <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -9,8 +22,8 @@ export const App = () => {
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
+      >
+      <Input value = {name} />
     </div>
-  );
+  )}
 };

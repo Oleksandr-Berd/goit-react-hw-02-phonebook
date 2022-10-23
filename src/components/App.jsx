e8e9191fbe72df932d206748c03b429de29df2e7
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-10-23 13:35:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,24 +22,6 @@ export class App extends React.Component {
       number,
     };
 
-    const contactArray = Object.values(this.state.contacts);
-
-    // this.setState(prevState => ({
-    //   contacts: prevState.contacts.map(el => {
-    //     el.name !== contact.name ?  contacts : [contact, ...prevState.contacts] : alert(`fck`)
-    //   }),
-    // }));
-
-    // this.setState(({ contacts }) => ({
-    //   contacts: contacts.map(el => {
-    //     if (el.id !== contact.id) {
-    //       return { contact, ...contacts };
-    //     } else {
-    //       alert(`FCK`);
-    //     }
-    //   }),
-    // }));
-
     if (this.state.contacts.some(contact => contact.name === name)) {
       alert(`contact ${name} already in contacts`);
       return;
@@ -48,16 +30,6 @@ export class App extends React.Component {
     this.setState(({ contacts }) => ({
       contacts: [...contacts, contact],
     }));
-
-    // this.setState(({ contacts }) => ({
-    //   contacts: contactArray.map(el => {
-    //     if (el.name === contact.name) {
-    //       return alert(`${contact.name} is already in contacts`);
-    //     } else {
-    //       return [contact, ...contacts];
-    //     }
-    //   }),
-    // }));
   };
 
   deleteContact = contactId => {
@@ -65,23 +37,6 @@ export class App extends React.Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
-
-  // toggleCompleted = contactId => {
-  //   console.log(contactId);
-
-  //   this.setState(({ contacts }) => ({
-  //     contacts: contacts.map(contact => {
-  //       if (contact.id === contactId) {
-  //         return {
-  //           ...contact,
-  //           name: !contact.name,
-  //           number: !contact.number,
-  //         };
-  //       }
-  //       return contact;
-  //     }),
-  //   }));
-  // };
 
   changeFilter = evt => {
     this.setState({ filter: evt.currentTarget.value });
